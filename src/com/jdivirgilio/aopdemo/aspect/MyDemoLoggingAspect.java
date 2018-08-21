4aa64743f971e683a4cb68fd051d37cb198310af
John DiVirgilio <lurkingmenace@gmail.com>
2018-08-21 10:11:21 -0500

    Added comments describing @Before and point-cut expressions

--- src/com/jdivirgilio/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/jdivirgilio/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,6 +12,7 @@ public class MyDemoLoggingAspect {
 	
 	// let's start with an @Before advice
 	@Before("execution(public void addAccount())") // The parenthetical exp is a pointcut expression
+	public void preAddAccount() {
 	  // A  predicate expression for where advice should be applied
 	  // An "execution" point cut applies to the execution of a given method
 	  // execution(modifiers-pattern? return-type-pattern declaring-type-patter? method-name-pattern(param-pattern) throws-pattern?)
@@ -19,7 +20,6 @@ public class MyDemoLoggingAspect {
 	  // The above @Before leaves off class name. It will match any calls with the sig above
 	  // @Before("execution(public void add*())") .. matches all methods starting with add....
 	  // @Before("execution(* add*())") .. minimum required w/ wildcards
-		
 		System.out.println(getClass() + ": beforeAddAccountAdvice()");
 	}
 	
