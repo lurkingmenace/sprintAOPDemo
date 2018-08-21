487243f0a4bd4ec19d29444477bdd92aecafd8ea
John DiVirgilio <lurkingmenace@gmail.com>
2018-08-21 11:35:08 -0500

    @Order demo

--- src/com/jdivirgilio/aopdemo/aspect/MyApiAnyalyticsAspect.java
+++ src/com/jdivirgilio/aopdemo/aspect/MyApiAnyalyticsAspect.java
@@ -2,10 +2,12 @@ package com.jdivirgilio.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(20)
 public class MyApiAnyalyticsAspect {
 
 	@Before("AopExpressions.forDaoPackageNoGetterSetter()")
--- src/com/jdivirgilio/aopdemo/aspect/MyCloudLogAsyncAspect.java
+++ src/com/jdivirgilio/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -2,10 +2,12 @@ package com.jdivirgilio.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(30)
 public class MyCloudLogAsyncAspect {
 	
 	@Before("AopExpressions.forDaoPackageNoGetterSetter()")
